32dd34d96494f51959ab7de57b9e571f1a44eadf
Victor Rodrigues <victorassisrd@gmail.com>
2018-02-01 10:58:29 -0200

    Tutorial 5
    Tutorial 5

--- PrjBiblioteca/Startup.cs
+++ PrjBiblioteca/Startup.cs
@@ -43,7 +43,7 @@ namespace PrjBiblioteca
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, BibliotecaDbContext bibliotecaContext)
         {
             if (env.IsDevelopment())
             {
@@ -65,6 +65,8 @@ namespace PrjBiblioteca
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            DbInitializer.Initialize(bibliotecaContext);
         }
     }
 }
